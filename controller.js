b3b4abd0a504e4410d8e8b17ba555d466ecddd7c
cstamper6 <you@example.com>
2019-12-08 15:43:23 -0500

    firebase tests

--- controller.js
+++ controller.js
@@ -73,4 +73,50 @@ function addObservers() {
 
 addObservers();
 
-// test
+
+
+// Firebase tests
+var database = firebase.database();
+
+// To read
+// database.ref('directory here')
+// ex
+
+function readData() {
+    database.ref('private/hashtags/count').on('value', function (snapshot) {
+        console.log(snapshot.val());
+    })
+}
+readData();
+
+// To write
+// database.ref('directory here').set({object})
+// ex
+
+function writeData() {
+    database.ref('public/').set({
+        username: 'cstamper',
+        food: 'sushi'
+    })
+}
+writeData();
+
+// To delete
+// database.ref('directory here').remove()
+// ex
+
+function deleteData(username, cat) {
+    database.ref('user/' + username + '/' + cat).remove()
+}
+deleteData('cstamper', 'favorites');
+
+// To update
+// database.ref('directory here').update()
+// ex
+
+function updateData() {
+    var updates = {};
+    updates['/count'] = 5000;
+    database.ref('private/hashtags/').update(updates)
+}
+updateData();
